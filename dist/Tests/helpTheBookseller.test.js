cf1e8ede1c07aeb077fc760151ab413861c03434
Bryan Oyloe <boyloe@gmail.com>
2020-12-21 10:49:38 -0700

    jest tests passing

--- dist/Tests/helpTheBookseller.test.js
+++ dist/Tests/helpTheBookseller.test.js
@@ -6,5 +6,10 @@ describe('takes array of books/values, and an array of categories;Returns number
     test('checks to see if inputs are empty arrays', () => {
         expect(G964.stockList([],[])).toBe("")
     })
-}
-)
+
+    test('returns category and value of all books in collection', () => {
+        expect(G964.stockList([ 'BBAR 150', 'CDXE 515', 'BKWR 250', 'BTSQ 890', 'DRTY 600' ],[ 'A', 'B', 'C', 'D' ]))
+            .toBe('(A : 0) - (B : 1290) - (C : 515) - (D : 600)')
+    })
+})
+
--- dist/helpTheBookseller.js
+++ dist/helpTheBookseller.js
@@ -8,4 +8,22 @@ G964.stockList = (listOfArt, listOfCat) => {
     if (listOfArt.length === 0 || listOfCat.length === 0) {
         return "";
     }
+    const categoryHashMap = new Map;
+    listOfCat.forEach(category => {
+        categoryHashMap.set(category, 0);
+    });
+    const splitArtList = listOfArt.map(entry => {
+        return entry.split(' ');
+    });
+    splitArtList.forEach(entry => {
+        categoryHashMap.set(entry[0][0], categoryHashMap.get(entry[0][0]) + parseInt(entry[1]));
+    });
+    const returnArray = [];
+    for (let [key, value] of categoryHashMap.entries()) {
+        returnArray.push(`(${key} : ${value})`);
+    }
+    return returnArray.join(' - ');
 };
+const listOfArt1 = ['BBAR 150', 'CDXE 515', 'BKWR 250', 'BTSQ 890', 'DRTY 600'];
+const listOfCat1 = ['A', 'B', 'C', 'D'];
+G964.stockList(listOfArt1, listOfCat1);
