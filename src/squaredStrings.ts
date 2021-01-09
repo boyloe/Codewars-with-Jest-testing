6c6922ac782a79fd507e0910aa095cfb6cb65621
Bryan Oyloe <boyloe@gmail.com>
2021-01-09 13:05:19 -0700

    Set up squaredStrings

---
+++ src/squaredStrings.ts
@@ -0,0 +1,11 @@
+export function rot(s: string): string {
+    return ""
+}
+
+export function selfieAndRot(s: string): string {
+    return ""
+}
+
+export function oper(fn: (s: string) => string, s: string): string {
+    return ""
+}
